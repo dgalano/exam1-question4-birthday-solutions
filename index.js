6ecad731437e5238b64b5f020d6e82a8181a3174
Debasis Bhattacharya <debasisb@hawaii.edu>
2022-02-14 23:00:44 +0000

    No changes!

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 //jshint esversion:6
 
-const zodiacSign = require('get-zodiac-sign');
-var myZodiacSign = zodiacSign(1,7);
-console.log(myZodiacSign);
+// Load module
+const calculator = require('birthday-calculator');
+ 
+// Instantiate
+const date = new Date('1965,01,07');
+const result = calculator.getDayWhenBorn(date);
+console.log(result);
 
 //use the fs function that comes with node
 const fs = require("fs");
-fs.writeFileSync("file1.txt", myZodiacSign);
+fs.writeFileSync("file1.txt", result);
